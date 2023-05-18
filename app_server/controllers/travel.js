4d67ac72cc5272c800a70368d18231d7f3ba510d
Jordan Heim <jordan.heim@snhu.edu>
2023-05-18 17:06:53 -0500

    Convert trips static HTML page to templates with JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,12 @@
+const fs = require('fs');
+
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.redner('travel', { title: 'Travlr Getaways'});
+    res.redner('travel', { title: 'Travlr Getaways', trips});
 };
+
 module.exports = {
     travel
 };
